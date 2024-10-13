f98ff17a093a6a8f634eb0c98d32e8693e87d7c2
CerroNegroCoder <jgonzalezcorte@uoc.edu>
2024-10-13 13:49:46 +0200

    Actualizacion splashactivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    //libreria de animacion
+    implementation ("com.airbnb.android:lottie:6.5.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
