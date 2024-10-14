655fb016b93e7cabfff19cb4f060f6fc109e87a7
Albert Lopez Pajuelo <alberto-1993@hotmail.es>
2024-10-14 20:31:55 +0200

    cambio de layout

--- app/src/main/java/com/projectgame/projectgame/InitialPag.java
+++ app/src/main/java/com/projectgame/projectgame/InitialPag.java
@@ -1,24 +1,29 @@
 package com.projectgame.projectgame;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class InitialPag extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_initial_pag);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Referencia al botón por su id
+        Button buttonStart = findViewById(R.id.button);
+
+        // Configura el onClickListener para el botón
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear el intent para navegar a SecondPag
+                Intent intent = new Intent(InitialPag.this, SecondPag.class);
+                startActivity(intent); // Lanza la nueva actividad
+            }
         });
     }
 }
