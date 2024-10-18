3e5a37131a64bbde5611b6306f44e6ec47b1e34d
Albert Lopez Pajuelo <alberto-1993@hotmail.es>
2024-10-18 10:57:23 +0200

    boton volver de crear cuenta

--- app/src/main/java/com/projectgame/projectgame/CrearCuentaActivity.java
+++ app/src/main/java/com/projectgame/projectgame/CrearCuentaActivity.java
@@ -1,5 +1,7 @@
+
 package com.projectgame.projectgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +19,7 @@ public class CrearCuentaActivity extends AppCompatActivity {
     private EditText editTextNuevaContraseña; // Si decides usar la contraseña
     private Button buttonRegistrar;
     private UserRepository userRepository; // Repositorio para manejar usuarios
+    private Button buttonVolver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,7 @@ public class CrearCuentaActivity extends AppCompatActivity {
         editTextNuevoUsuario = findViewById(R.id.editTextNuevoUsuario);
         editTextNuevaContraseña = findViewById(R.id.editTextNuevaContraseña);
         buttonRegistrar = findViewById(R.id.buttonRegistrar);
+        buttonVolver = findViewById(R.id.buttonVolver); // Inicializar el botón volver
 
         // Inicializar el repositorio de usuarios
         userRepository = new UserRepository(this);
@@ -54,6 +58,16 @@ public class CrearCuentaActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(CrearCuentaActivity.this, "Por favor ingresa un nombre de usuario y una contraseña", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        // Configurar el botón para volver a la pantalla inicial
+        buttonVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear un Intent para redirigir a la actividad InitialPag
+                Intent intent = new Intent(CrearCuentaActivity.this, SecondPag.class);
+                startActivity(intent); // Iniciar la nueva actividad
             }
         });
     }
