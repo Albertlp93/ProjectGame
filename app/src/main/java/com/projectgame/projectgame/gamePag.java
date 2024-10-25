e8d7bca7753e699beb39e0866892b69acf2bfded
Albert Lopez Pajuelo <alberto-1993@hotmail.es>
2024-10-25 16:02:37 +0200

    prueba subida

--- app/src/main/java/com/projectgame/projectgame/gamePag.java
+++ app/src/main/java/com/projectgame/projectgame/gamePag.java
@@ -65,7 +65,7 @@ public class gamePag extends AppCompatActivity {
             if (playerBet == 0) {
                 Toast.makeText(this, "Debe realizar una apuesta primero", Toast.LENGTH_SHORT).show();
             } else if (playerCoins < 5) { // Verifica si tiene suficientes monedas
-                Toast.makeText(this, "No tienes suficientes monedas para apostar", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "No tiene suficientes monedas para apostar", Toast.LENGTH_SHORT).show();
             } else {
                 playerCoins -= 5;
                 updateCoinsDisplay();
