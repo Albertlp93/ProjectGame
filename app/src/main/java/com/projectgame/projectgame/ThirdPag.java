ecc3d40475ff47dc50a7c98bd175bb8c5ed51bd4
CerroNegroCoder <jgonzalezcorte@uoc.edu>
2024-11-06 17:41:01 +0100

    Musica Oficial de fondo

--- app/src/main/java/com/projectgame/projectgame/ThirdPag.java
+++ app/src/main/java/com/projectgame/projectgame/ThirdPag.java
@@ -1,52 +1,60 @@
 package com.projectgame.projectgame;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+
 public class ThirdPag extends AppCompatActivity {
-    //ATRIBUTOS
+    // ATRIBUTOS
     private String nombreUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //LAYOUT
+        // LAYOUT
         setContentView(R.layout.activity_third_pag);
 
-        //INICIALIZAR BOTONES
-        Button buttonIniciar = findViewById(R.id.buttonIniciar);     //Iniciar juego
-        Button buttonHistorico = findViewById(R.id.buttonHistorico); //Historico juego
+        // INICIALIZAR BOTONES
+        Button buttonIniciar = findViewById(R.id.buttonIniciar);     // Iniciar juego
+        Button buttonHistorico = findViewById(R.id.buttonHistorico); // Histórico juego
 
-        //OBTENER - Nombre Usuario
+        // OBTENER - Nombre Usuario
         nombreUsuario = getIntent().getStringExtra("nombreUsuario");
 
-        //BOTON - INICIAR JUEGO
+        // BOTON - INICIAR JUEGO
         buttonIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                //MOVER A LA SIGUIENTE PAGINA {gamePag}
+                // MOVER A LA SIGUIENTE PAGINA {gamePag}
                 Intent intent = new Intent(ThirdPag.this, gamePag.class);
-                    //Pasar el nombre de usuario a la siguiente actividad
-                    intent.putExtra("nombreUsuario", nombreUsuario);
-                    startActivity(intent);
+                // Pasar el nombre de usuario a la siguiente actividad
+                intent.putExtra("nombreUsuario", nombreUsuario);
+                startActivity(intent);
             }
         });
 
-        //BOTON - HISTORICO
+        // BOTON - HISTORICO
         buttonHistorico.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                //MOVER A LA SIGUIENTE PAGINA {HistoricalPag}
+                // MOVER A LA SIGUIENTE PAGINA {HistoricalPag}
                 Intent intent = new Intent(ThirdPag.this, HistoricalPag.class);
                 intent.putExtra("nombreUsuario", nombreUsuario);
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Detener la música al entrar en esta actividad
+        if (InitialPag.mediaPlayer != null) {
+            InitialPag.mediaPlayer.pause(); // Pausar la música
+            InitialPag.isMusicPlaying = false; // Actualiza el estado de la música
+        }
     }
 }
